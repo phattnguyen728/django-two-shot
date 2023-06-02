f84c76691bb69efec020f85db9ae166be666e6f2
Phat <ptnguyen728@gmail.com>
2023-06-01 21:54:53 -0500

    Feature 11 Complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from receipts.views import (
     receipt_list,
+    create_receipt,
 )
 
 urlpatterns = [
     path("", receipt_list, name="home"),
+    path("create/", create_receipt, name="create_receipt")
     ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from receipts.models import Receipt
 from django.contrib.auth.decorators import login_required
+from receipts.forms import ReceiptForm
 # Create your views here.
+
+
+@login_required
+def create_receipt(request):
+    if request.method == "POST":
+        form = ReceiptForm(request.POST)
+        if form.is_valid():
+            #Creates an object but doesn't save
+            receipt = form.save(False)
+            receipt.purchaser = request.user
+            #saves here
+            receipt.save()
+            return redirect("home")
+    else:
+        form = ReceiptForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "receipts/create.html", context)
 
 
 @login_required
