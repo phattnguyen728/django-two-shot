cf948250363cf3e8573ce1189d4ff9a4ce6b6d1c
Phat <ptnguyen728@gmail.com>
2023-06-01 20:52:51 -0500

    Feature 10 Complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render, redirect
-from accounts.forms import LogInForm
+from accounts.forms import LogInForm, SignUpForm
 from receipts.models import Account
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+
+def user_signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            password_confirmation = form.cleaned_data["password_confirmation"]
+            if password == password_confirmation:
+                user = User.objects.create_user(
+                    username=username,
+                    password=password,
+                    )
+                login(request, user)
+                return redirect("home")
+            else:
+                form.add_error("password", "the passwords do not match")
+    else:
+        form = SignUpForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
+
+
 
 
 def user_login(request):
--- receipts/urls.py
+++ receipts/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from receipts.views import (
     receipt_list,
 )
 
 urlpatterns = [
     path("", receipt_list, name="home"),
-]
+    ]
